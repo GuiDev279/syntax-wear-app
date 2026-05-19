0fb34f4b239f78c5512964400c5878cf349d3b0a
Guilherme Carvalho <guidev279@gmail.com>
2026-05-19 10:53:45 -0300

    feat: adiciona componente de button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+
+
+interface ButtonProps {
+    children: React.ReactNode
+    variant?: 'primary' | 'secondary'
+    size?: 'sm' | 'md' | 'lg'
+}
+
+export const Button = ({ children, variant = 'primary', size = 'md' }: ButtonProps) => {
+
+    const buttonstyles = {
+        base: 'flex justify-center items-center gap-2 text-nowrap leading-none hover:cursor-pointer transition-colors duaration-200 font-medium rounded-full transition py-2.5',
+        variant: {
+            primary: 'bg-white text-[#6329A2] hover:bg-gray-100',
+            secondary: 'bg-transparent border border-white text-white hover:bg-white hover:text-black',
+        },
+        size: {
+            sm: 'px-5 ',
+            md: 'px-8 ',
+            lg: 'px-10 ',
+        }
+    }
+
+    const className = `${buttonstyles.base} ${buttonstyles.variant[variant]} ${buttonstyles.size[size]}`
+
+    return (
+        <button className={className}>
+            {children}
+        </button>
+    )
+}
